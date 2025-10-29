85743de6d3846ec78a916ae50ee2f1a185ed5f7b
Jimut Mahato <69151735+jimut60@users.noreply.github.com>
2025-10-29 12:30:07 +0530

    Create metaploit.py

---
+++ metaploit.py
@@ -0,0 +1,13 @@
+# importing packages 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+# loading dataset 
+data = sns.load_dataset("iris") 
+
+# draw lineplot 
+sns.lineplot(x="sepal_length", y="sepal_width", data=data) 
+
+# changing the theme to dark
+sns.set_style("dark")
+plt.show()
